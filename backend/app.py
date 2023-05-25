8e5e37d1b0e3c05246b30826177987a15f686a76
achyutbenz19 <achyut.benz@gmail.com>
2023-05-25 18:34:12 -0500

    mongo db works

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,26 @@
 from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo, ObjectId
+from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost/test'
+app.config['MONGO_URI'] = 'mongodb://localhost/fullstackapp'
 mongo = PyMongo(app)
 
 CORS(app)
 
 db = mongo.db.users
-@app.route("/", methods = ['GET'])
-def main():
-    return '<h1>Hello World!</h1'
+
+@app.route("/users", methods=['POST'])
+def create_user():
+    user_data = {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'phone': request.json['phone'],
+        'address': request.json['address']
+    }
+    result = db.insert_one(user_data)
+    inserted_id = str(result.inserted_id)
+    return jsonify({'id': inserted_id, 'msg': 'User added successfully.'})
 
 if __name__ == "__main__":
     app.run(debug=True)
