b6fe5ee334a23b5c732689e016a84ff363d2e747
achyutbenz19 <achyut.benz@gmail.com>
2023-05-30 15:40:39 -0500

    actions

---
+++ frontend/src/redux/actions.js
@@ -0,0 +1,15 @@
+import * as types from "./actionTypes";
+import axois from "axois";
+
+const API = "http://127.0.0.1:5000"
+
+const getUser = () => ({
+    type: types.GET_USERS,
+    payload: users
+});
+
+export const loadUsers = () => {
+    return function(dispatch) {
+    axois.get('${API}/users').then((resp) => dispatch(getUser(resp.data))).catch(err => console.log(err))
+    }
+}
