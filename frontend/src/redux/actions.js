21e32579075977e55d9e59b537816ffc2074eb8f
achyutbenz19 <achyut.benz@gmail.com>
2023-05-30 16:05:14 -0500

    minor

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -1,16 +1,16 @@
 import * as types from "./actionTypes";
-import axois from "axois";
+import axios from "axios";
 
 const API = "http://127.0.0.1:5000"
 
-const getUser = () => ({
+const getUser = (users) => ({
     type: types.GET_USERS,
     payload: users
 });
 
 export const loadUsers = () => {
     return function(dispatch) {
-        axois
+        axios
         .get('${API}/users')
         .then((resp) => dispatch(getUser(resp.data)))
         .catch(err => console.log(err));
