8a0030435177c32d6feb26a8ed64517c6d15c7ee
achyutbenz19 <achyut.benz@gmail.com>
2023-05-30 15:55:38 -0500

    minor

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Navbar, Table, Container, Row, Col, Button, ButtonGroup, Form} from "react-bootstrap";
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUsers } from './redux/actions';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(()=> {
+    dispatch(loadUsers());
+  }, [])
+
   return (
     <Navbar bg = "primary" variant = "dark" className = "justify-content-center">
         <Navbar.Brand>Python Flask MongoDB Redux App</Navbar.Brand>
