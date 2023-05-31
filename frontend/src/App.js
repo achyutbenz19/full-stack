e539135cdebce20c252412283fa86d768e9e8fb5
achyutbenz19 <achyut.benz@gmail.com>
2023-05-30 20:03:48 -0500

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/users", {
-      "methods": "GET",
+      "method": "GET",
       headers: {
         'Content-Type': 'application/json'
       }
