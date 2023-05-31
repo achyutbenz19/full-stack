588742c33d150a44c6b2eaf4ee7eb21a1a016e0d
achyutbenz19 <achyut.benz@gmail.com>
2023-05-30 20:02:09 -0500

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,8 +22,11 @@ function App() {
 
       {datas.map(data => {
         return (
-          <div key = {data.id}>
+          <div key = {data._id}>
             <h2>{data.name}</h2>
+            <h3>{data.phone}</h3>
+            <h3>{data.address}</h3>
+            <h3>{data.email}</h3>
           </div>
         )
       })}
