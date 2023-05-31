a8c187e9487dfd831b90eb68ea7b815eb1f3cbfc
achyutbenz19 <achyut.benz@gmail.com>
2023-05-30 19:56:39 -0500

    end points connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,21 @@
+import React, { useEffect, useState } from "react";
+
 function App() {
+
+  const [data, setdata] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/users", {
+      "methods": "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <h1>Flask MonogoDB</h1>
